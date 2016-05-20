6ce068f12e0b4b68328c9ed6fc61d65e6e1df066
P. Colin Manikoth <colin@manikoth.com>
2016-05-20 11:26:16 -0700

    Base Commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// First, load jQuery (required)
+// jQuery 2.2.2 loaded via `index.html`
+
+// Second, load Semantic UI JS (required)
+// @codekit-prepend "semantic.js";
+
+// Third, Add to Homescreen (optional)
+// @codekit-prepend "add-to-homescreen.js";
+
+// Third, load any plugins (optional)
+// @codekit-prepend "plugins.js";
+
+// Add to Home Screen
+addToHomescreen();
+
+// My Scripts
